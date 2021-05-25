d10acef19a4b9a6a0c6462ea48a6a6f8666167f6
Asanga Udara <70792747+asangaudara@users.noreply.github.com>
2021-05-25 22:17:58 +0530

    Update video.py

--- DaisyX/modules/video.py
+++ DaisyX/modules/video.py
@@ -33,9 +33,9 @@ async def download_video(v_url):
     sender = await lazy.get_sender()
     me = await lazy.client.get_me()
     if not sender.id == me.id:
-        rkp = await lazy.reply("`processing...`")
+        rkp = await lazy.reply("**processing...**")
     else:
-        rkp = await lazy.edit("`processing...`")
+        rkp = await lazy.edit("**processing...**")
     url = v_url.pattern_match.group(1)
     if not url:
         return await rkp.edit("`Error \nusage song <song name>`")
@@ -48,7 +48,7 @@ async def download_video(v_url):
     except:
         return await rkp.edit("`failed to find`")
     type = "audio"
-    await rkp.edit("`Preparing to download...`")
+    await rkp.edit("**Preparing to download...**")
     if type == "audio":
         opts = {
             "format": "best",
@@ -67,7 +67,7 @@ async def download_video(v_url):
         song = False
         video = True
     try:
-        await rkp.edit("`Fetching data, please wait..`")
+        await rkp.edit("**Downloading My Sever Pleas Wait 😊**")
         with YoutubeDL(opts) as rip:
             rip_data = rip.extract_info(url)
     except DownloadError as DE:
